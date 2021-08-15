809601d5aef8ef8c36a871deba263e229257b0e0
Тимур Гиреев <gireev2003@mail.ru>
2021-08-15 16:00:58 +0300

    Создание основной модели представления и добавление прицела для тестирования

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CrosshairDesktopWPF.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrosshairDesktopWPF.ViewModels
+{
+    class MainWindowViewModel : BaseVM
+    {
+
+
+
+    }
+}
