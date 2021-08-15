7c00c4ae708ff836e6b511380aa9e6b359e1bd84
Тимур Гиреев <gireev2003@mail.ru>
2021-08-15 15:57:38 +0300

    Создание базового класса модели представления и перенос главного представления в соответствующую папку.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CrosshairDesktopWPF
+namespace CrosshairDesktopWPF.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
