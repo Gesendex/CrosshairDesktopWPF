65f65fb7326ff9ac143e35c594bfb6c8933551d5
Тимур Гиреев <gireev2003@mail.ru>
2021-08-15 19:43:18 +0300

    Создано основное меню и добавлена возможность задавать разрешение экрана

---
+++ Models/CrosshairModels/Crosshair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CrosshairDesktopWPF.Models.CrosshairModels
+{
+    class Crosshair
+    {
+        public Image CrosshairImage { get; set; }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CrosshairDesktopWPF.Core;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,23 @@ namespace CrosshairDesktopWPF.ViewModels
 {
     class MainWindowViewModel : BaseVM
     {
+        public MainWindowViewModel()
+        {
+            DisplaySize = new Size(1920, 1080);
+        }
 
+        private Size DisplaySize;
 
+        public int DisplayWidth
+        {
+            get { return DisplaySize.Width; }
+            set { DisplaySize.Width = value; OnPropertyChanged(); }
+        }
 
+        public int DisplayHeight
+        {
+            get { return DisplaySize.Height; }
+            set { DisplaySize.Height = value; OnPropertyChanged(); }
+        }
     }
 }
